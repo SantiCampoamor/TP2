c75684a542534f2a74a60f96b7a617728ee10786
SantiCampoamor <101577703+SantiCampoamor@users.noreply.github.com>
2022-04-19 09:58:21 -0300

    Script unity video1 finished

--- Assets/Scripts/Video1.cs
+++ Assets/Scripts/Video1.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class Video1 : MonoBehaviour
 {
+    public int edadUsuario; 
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        if (edadUsuario >= 18)
+        {
+            Debug.Log("Es mayor de edad");
+        }
+        else
+        {
+            Debug.Log("No es mayor de edad");
+        }
     }
 
     // Update is called once per frame
